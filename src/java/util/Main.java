94850168e95e104e16aaccc6a620bdd79c8552d6
Stepan Lozhkin <stepanl123@yandex.ru>
2024-06-09 16:34:14 +0300

    Add random

---
+++ src/java/util/Main.java
@@ -0,0 +1,12 @@
+package java.util;
+public class Main()
+{
+    public void Main(String[] args)
+    {
+        System.out.println("Choose the symbol!");
+        System.out.println("1 - rock, 2 - paper, 3 - scissors");
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+
+    }
+}
